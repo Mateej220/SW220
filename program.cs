ae4db4e7781274171952645cf46a35a8d0b6310e
Mateej220 <matisek.sk@outlook.com>
2025-09-08 20:13:27 +0200

    Modified main program to test drawing functions of the panel.

--- program.cs
+++ program.cs
@@ -7,8 +7,9 @@
 			// Program entrance //
 			Console.WriteLine("--- SW220 LIB ---");
 
-			Dialogs.Draw.Head(16, 4, 48, "About SW220");
-			Dialogs.Draw.BodyMessage(16, 4, 48, new string[] {
+			int i;
+			Dialogs.Draw.Head(16, 4, 48, "About library: SW220");
+			i = Dialogs.Draw.BodyMessage(16, 4, 48, new string[] {
 				"SW220 is a C# library for building console",
 				"applications with advanced user interface.",
 				"",
@@ -17,6 +18,11 @@
 				"",
 				"(c) 2025 SW220 contributors"
 			});
+
+			i = Dialogs.Draw.PanelHead(16, 15, 4 - i + 1, 48);
+			i = Dialogs.Draw.PanelBody(16, 15, i, 48, 0);
+			Dialogs.Draw.PanelEnd(16, 15, i, 48);
+
 
 			// Exit //
 			Console.ReadKey();
