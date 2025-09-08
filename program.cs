46a10dc3cdf7c7ba7116a692e3b5096c01e8d706
Mateej220 <matisek.sk@outlook.com>
2025-09-08 19:31:06 +0200

    Modified main program to test drawing functions of the library.

--- program.cs
+++ program.cs
@@ -7,7 +7,16 @@
 			// Program entrance //
 			Console.WriteLine("--- SW220 LIB ---");
 
-			Dialogs.Draw.Head(16, 4, 48, "Configuration");
+			Dialogs.Draw.Head(16, 4, 48, "About SW220");
+			Dialogs.Draw.BodyMessage(16, 4, 48, new string[] {
+				"SW220 is a C# library for building console",
+				"applications with advanced user interface.",
+				"",
+				"It is based on Spectre.Console library",
+				"(https://spectreconsole.net/).",
+				"",
+				"(c) 2025 SW220 contributors"
+			});
 
 			// Exit //
 			Console.ReadKey();
