91fa823c0be1cb79b34c498263ca057a2905eabf
Mateej220 <matisek.sk@outlook.com>
2025-09-10 19:58:41 +0200

    Added support for drawing end of the menu.

--- source/dialogs/graphics/menu.cs
+++ source/dialogs/graphics/menu.cs
@@ -68,8 +68,8 @@ namespace SW220
                     else
                     {
                         Message = new Markup(
-                        theme.text_backlight + i.Key + "  [/]"	+
-						theme.text_normal + i.Value + "[/]"
+                        theme.text_backlight + i.Key + "  [/]" +
+                        theme.text_normal + i.Value + "[/]"
                         );
                     }
 
@@ -83,6 +83,33 @@ namespace SW220
                     pos_y++;
                     drawn++;
                 }
+
+                // Return number of lines that were drawn	//
+                // (to be able to calculate next position)	//
+                return pos_y;
+            }
+
+            public static int MenuEnd(int x, int y, int pos_y, int width, Theme? theme = null)
+            {
+                // If there is no defined theme, dialog will use default one.	//
+                // (New instance of "Theme" [file: source/dialogs/theme.cs] )	//
+                theme ??= new Theme();
+
+                // Style definitions //
+                var Frame = new Markup(
+                    "[white on silver]│ [/]" +
+                    "[black on silver]└[/]" +
+                    "[white on silver]" +
+                    General.Repeat("─", width - 6) +
+                    "┘[/][black on silver] [/]" +
+                    "[black on silver]│[/]" +
+                    "[black on black]  [/]"
+                );
+
+                // Draw end of the menu //
+                AnsiConsole.Cursor.SetPosition(x, y + pos_y);
+                AnsiConsole.Write(Frame);
+                pos_y++;
 
                 // Return number of lines that were drawn	//
                 // (to be able to calculate next position)	//
