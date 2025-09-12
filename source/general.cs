58867b030d1cb6c286683c05d8628cdf5fe77877
Mateej220 <matisek.sk@outlook.com>
2025-09-12 18:39:26 +0200

    Add comment.

--- source/general.cs
+++ source/general.cs
@@ -1,11 +1,12 @@
 namespace SW220
 {
-    public static class General
-    {
-        public static string Repeat(string text, int amount)
-        {
-            string output = string.Concat(Enumerable.Repeat(text, amount));
-            return output;
-        }
-    }
+	public static class General
+	{
+		// --- General purpose functions --- //
+		public static string Repeat(string text, int amount)
+		{
+			string output = string.Concat(Enumerable.Repeat(text, amount));
+			return output;
+		}
+	}
 }
